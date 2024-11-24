1320f74e4b7541e72205f25f222b0ba751592a69
Osminalx <verdestro64@gmail.com>
2024-11-24 13:21:25 -0800

    Devops Scripts fix RQ05

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,11 @@ output "backend_app_name" {
 output "backend_app_url" {
   value = "https://${azurerm_linux_web_app.django_app.default_hostname}"
 }
+
+output "database_url" {
+  value = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name};Database=${azurerm_mssql_database.database.name};User Id=${var.sql_admin_username};Password=${var.sql_admin_password};"
+  sensitive = true
+}
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
